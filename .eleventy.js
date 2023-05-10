cc4d10992fd4b290408ca51432bcfaa7f11efb13
JeremyJamesL <jeremyluscombe@gmail.com>
2023-05-10 19:20:24 +0100

    fixed sass watch

--- .eleventy.js
+++ .eleventy.js
@@ -2,12 +2,12 @@ const pluginSEO = require("eleventy-plugin-seo");
 
 module.exports = function(eleventyConfig) {
     // Watch CSS files for changes
-    eleventyConfig.setBrowserSyncConfig({
-		files: './site/css/**/*.css'
-	});
+    eleventyConfig.addWatchTarget("./src/sass/");
 
+    // Add support for SEO
     eleventyConfig.addPlugin(pluginSEO, require("./src/_data/site.json"))
 
+    // Change default input directory
     return {
         dir: {
             input: "src"
